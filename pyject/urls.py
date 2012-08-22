1a495eea4adb77e814ecb8b89f5b112ab3606ea6
Hannes Lohmander <hannes@lohmander.me>
2012-08-22 22:02:37 +0200

    Added views and stuff

--- pyject/urls.py
+++ pyject/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pyject.views.home', name='home'),
     # url(r'^pyject/', include('pyject.foo.urls')),
+    url(r'^users/login/$', 'users.views.login')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
