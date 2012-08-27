4ea6476f8b77ff63b97738f7718bf944a138f3ef
Hannes Lohmander <hannes@lohmander.me>
2012-08-27 22:02:27 +0200

    Made a less braindead solution to login functionality and added temporary stylesheet to make it more bearable to work with.
    Final design to be decided upon soon enough.

--- users/models.py
+++ users/models.py
@@ -10,3 +10,18 @@ class User(models.Model):
 
     def __unicode__(self):
         return self.username
+
+
+class Auth(models.Model):
+    def initiate(self, username, password):
+        username_auth = User.objects.filter(username__exact=username, password__exact=password)
+        email_auth = User.objects.filter(email__exact=username, password__exact=password)
+
+        if username_auth:
+            user_id = username_auth[0].id
+            return user_id
+        elif email_auth:
+            user_id = email_auth[0].id
+            return user_id
+        else:
+            return False
--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from users.models import User
+from users.models import Auth
 
 
 def login(request):
@@ -11,26 +11,19 @@ def login(request):
 
 
 def validate(request):
-    get_object_or_404(User)
-
     username = request.POST['email']
     password = request.POST['password']
 
-    with_username = User.objects.filter(username__exact=username, password__exact=password)
-    with_email = User.objects.filter(email__exact=username, password__exact=password)
+    a = Auth()
+    valid = a.initiate(username, password)
 
-    if with_username:
-        success = with_username
-    elif with_email:
-        success = with_email
+    if valid:
+        request.session['user_id'] = valid
+        return HttpResponseRedirect(reverse('users.views.dash'))
     else:
         return render_to_response('users/login.html', {
             'error_message': 'Wrong username or password.'
             }, context_instance=RequestContext(request))
-
-    if success:
-        request.session['user_id'] = success
-        return HttpResponseRedirect(reverse('users.views.dash'))
 
 
 def dash(request):
